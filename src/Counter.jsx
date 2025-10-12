dce7545fd57dc59b6d752c8c526e24c43ab60101
Talha <talhaaptech12@gmail.com>
2025-10-13 00:37:35 +0500

    till 3 hrs video

---
+++ src/Counter.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+export default function Counter() {
+    const [count, setCount] =useState(0);
+
+  return (
+    <>
+    <h1>Counter</h1>
+    <h2>Counter: {count}</h2>
+    <button onClick={()=>setCount(count + 1)}>Add numbers</button>
+  </>)
+}
---
+++ src/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function User({name}) {
+  return (
+    <div>
+        <h1>haleema</h1>
+
+<h2>Student name: {name}</h2>
+
+
+        </div>
+  )
+}
