87dea16d7b384bdd619d8840eb70aa5459cd6a64
Talha <talhaaptech12@gmail.com>
2025-10-15 00:12:56 +0500

    this is till 4 hrs

---
+++ src/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function User({data}) {
+  return (
+    <div>
+        <h3>Name: {data.Name}</h3>
+        <h3>Age: {data.Age}</h3>
+
+
+
+    </div>
+  )
+}
