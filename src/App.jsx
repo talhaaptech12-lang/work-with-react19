be36fbd48495e6778e1bc7ff440a1982b6390f68
Talha <talhaaptech12@gmail.com>
2025-10-21 23:22:37 +0500

    this is till 7:23hrs

--- src/App.jsx
+++ src/App.jsx
@@ -5,41 +5,23 @@ import './App.css'
 
 
 function App() {
-  const [data, setdata]=useState({
-  
-    Name: "Talha",
-    Address:{
-      City: "Karachi",
-      Country: "Pakistan"
+  const [data, setdata]=useState(["talha", "ali", 'farhan'])
 
-    
-  }
-})
-const handleName=(val)=>{
-  data.Name=val
-  setdata({...data})
+const handleName=(name)=>{
+data[data.length-1]=name
+setdata([...data])
+console.log(data)
 }
 
-
-const handleCity=(City)=>{
-  data.Address.City=City
-  console.log(data)
-  setdata({...data, Address:{...data.Address,City}})
-}
-const handleCountry=(Country)=>{
-  data.Address.Country=Country
-  // console.log(data)
-  setdata({...data, Address:{...data.Address,Country}})
-}
   return (
     <>
     <input type="text"  onChange={(event)=>handleName(event.target.value)} placeholder='Update your data' />
-    <input type="text"  onChange={(event)=>handleCity(event.target.value)} placeholder='Update your data' />
-    <input type="text"  onChange={(event)=>handleCountry(event.target.value)} placeholder='Update your data' />
-    <h3>Name: {data.Name} </h3>
-    <h3>City: {data.Address.City} </h3>
-    <h3>Country: {data.Address.Country} </h3>
-
+ 
+{
+  data.map((item, index)=>(
+<h3 key={index}>{item}</h3>
+  ))
+}
     
 
   
